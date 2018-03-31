6112cc99096d0745d49e169d2fa665052ac833c9
Javier Garcia <javiergarciaramos@gmail.com>
2018-03-31 12:44:09 +0100

    Initial commit

---
+++ ProductsWeb/ProductsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductsWeb.Startup))]
+namespace ProductsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
