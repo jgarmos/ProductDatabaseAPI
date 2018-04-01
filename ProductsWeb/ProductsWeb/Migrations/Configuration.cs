8a13661afff165bd4721f898bbaf4ab74e29732f
Javier Garcia <javiergarciaramos@gmail.com>
2018-04-01 09:53:29 +0100

    Seed database from json initial data

--- ProductsWeb/ProductsWeb/Migrations/Configuration.cs
+++ ProductsWeb/ProductsWeb/Migrations/Configuration.cs
@@ -4,6 +4,13 @@ namespace ProductsWeb.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Models;
+    using System.IO;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using System.Web;
+    using System.Web.Hosting;
+    using System.Reflection;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ProductsWeb.Models.ProductsDbContext>
     {
@@ -12,20 +19,27 @@ namespace ProductsWeb.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(ProductsWeb.Models.ProductsDbContext context)
+        protected override void Seed(ProductsDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            using (StreamReader r = new StreamReader("C:/Users/Javier/Source/Repos/ProductDatabaseAPI/ProductsWeb/ProductsWeb/App_Data/products.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
+                foreach (var product in products)
+                {
+                    context.Products.AddOrUpdate(p => p.Id,
+                    new Product()
+                    {
+                        Id = products.Count,
+                        Name = product.Name,
+                        Description = product.Description,
+                        Category = product.Description
+                    }
+                    );
+                }
+            }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
+
     }
 }
